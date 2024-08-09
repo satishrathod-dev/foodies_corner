4460d573d400a4e823b07805fbed5752ec61c44f
Satish Rathod <satishrathod.dev@gmail.com>
2024-08-09 06:20:21 +0530

    proxy removed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const MENU_API =
-  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.61610&lng=73.72860&restaurantId=";
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.61610&lng=73.72860&restaurantId=";
